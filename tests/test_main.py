4aacfbc3f1fbbc569eb55349b440af0fefcd6ab8
hariarul <hariarul344@gmail.com>
2025-05-15 12:13:10 +0530

    modified files are added

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
+# tests/test_main.py
 from src.main import add
 
-def add_test_func(a,b):
-    assert add(1,3)==4
-    assert add(0,0)==0
-    assert add(4,1)==5
+def test_add():
+    assert add(1, 3) == 4
+    assert add(0, 0) == 0
+    assert add(4, 1) == 5
